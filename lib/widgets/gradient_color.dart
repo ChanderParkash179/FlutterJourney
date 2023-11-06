224155be264bf6d15a703cb156e8de1291834023
Chander Parkash <chanderparkash179@gmail.com>
2023-11-06 13:47:32 +0500

    learned variables, const and final

--- lib/widgets/gradient_color.dart
+++ lib/widgets/gradient_color.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_journey/tasks/custom_text_widget_01.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 81, 13, 177),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
