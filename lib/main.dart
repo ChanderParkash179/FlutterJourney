103e39e8cfba32cb63440421455ffd9a3fa916ad
Chander Parkash <chanderparkash179@gmail.com>
2023-11-06 13:30:53 +0500

    learnt MaterialApp, Scaffold, Container, Center, Text Widgets and there properties

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 81, 13, 177),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 32,
+                fontFamily: 'Times New Roman',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
