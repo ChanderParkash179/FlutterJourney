b6b1400ba33a792e9175f3ad4f081093994d4380
Chander Parkash <chanderparkash179@gmail.com>
2023-11-14 22:46:55 +0500

    started expense tracker [PROJECT-03] part-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_journey/projects/quiz/quiz.dart';
+import 'package:flutter_journey/projects/expense_tracker/expenses.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Quiz(),
+        body: Expenses(),
       ),
     );
   }
