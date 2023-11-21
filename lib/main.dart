ca82d8cf97f4a73c258956485822009df640beb2
Chander Parkash <chanderparkash179@gmail.com>
2023-11-21 18:21:09 +0500

    separated main method code for project 03 in a widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_journey/projects/expense_tracker/widgets/expenses.dart';
+import 'package:flutter_journey/widgets/expense_tracker_main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,65 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var kColorScheme = ColorScheme.fromSeed(
-      seedColor: const Color.fromARGB(255, 96, 59, 181),
-    );
-
-    var kDarkColorScheme = ColorScheme.fromSeed(
-      seedColor: const Color.fromARGB(255, 5, 99, 125),
-      brightness: Brightness.dark,
-    );
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16,
-              ),
-            ),
-      ),
-      themeMode: ThemeMode.light,
-      home: const Scaffold(
-        body: Expenses(),
-      ),
-    );
+    return const ExpenseTrackerMain();
   }
 }
